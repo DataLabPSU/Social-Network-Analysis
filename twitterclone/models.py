71866b03fbac10005fc040fd37676e1a05cba25b
Adrian Kato <adriankato@gmail.com>
2018-10-04 17:42:22 -0400

    working on left bar

--- twitterclone/models.py
+++ twitterclone/models.py
@@ -5,46 +5,62 @@ from django.dispatch import receiver
 from django.contrib.auth.models import User
 import uuid
 # Create your models here.
+
+
 class Message(models.Model):
-	author = models.ForeignKey(User, on_delete = models.CASCADE)
-	message = models.TextField()
-	recipient = models.CharField(max_length=100)
-	def __str(self):
-		return self.message
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    recipient = models.CharField(max_length=100)
+
+    def __str(self):
+        return self.message
+
+
 class Post(models.Model):
-	author = models.ForeignKey(User, on_delete=models.CASCADE)
-	id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
-	title = models.CharField(max_length=100)
-	text = models.TextField()
-	created_date = models.DateTimeField(
-			default=timezone.now)
-	likes = models.IntegerField(default=0)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    likes = models.IntegerField(default=0)
 
-	def publish(self):
-		self.save()
-	def __str__(self):
-		return self.title
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
 class Share(models.Model):
-	shared = models.ForeignKey(User,blank=True, null=True, on_delete=models.CASCADE)
-	postid = models.TextField()
-	date = models.DateTimeField(default=timezone.now)
-class Comment(models.Model):
-	post = models.ForeignKey(Post,blank=True, null=True, on_delete=models.CASCADE)
-	text = models.TextField()
-	author = models.ForeignKey(User, on_delete=models.CASCADE,blank=True, null=True)
+    shared = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    postid = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
 
-	created_date = models.DateTimeField(default=timezone.now)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, blank=True, null=True, on_delete=models.CASCADE)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+
+    created_date = models.DateTimeField(default=timezone.now)
+
+
 class Profile(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	bio = models.TextField(max_length=500,blank=True)
-	following = models.TextField(max_length=1000,blank=True)
-	liked = models.TextField(max_length=1000,blank=True)
-	notifications = models.IntegerField(default=1)
-@receiver(post_save,sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-	if created:
-		Profile.objects.create(user=instance)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    following = models.TextField(max_length=1000, blank=True)
+    liked = models.TextField(max_length=1000, blank=True)
+    notifications = models.IntegerField(default=1)
+    notificationsString = models.TextField(default='')
+
 
 @receiver(post_save, sender=User)
-def save_user_profile(sender,instance,**kwargs):
-	instance.profile.save()
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
--- twitterclone/views.py
+++ twitterclone/views.py
@@ -1,106 +1,121 @@
-from django.shortcuts import render,redirect,render_to_response,get_object_or_404
+from django.shortcuts import render, redirect, render_to_response, get_object_or_404
 from django.template import RequestContext
-from .models import Post,Comment,Share
+from .models import Post, Comment, Share
 from . import forms
 from django.contrib.auth.models import User
 from django.http import HttpResponse
-from django.contrib.auth import login,authenticate
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+
+
 def home(request):
-	if request.user.is_authenticated:
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            try:
+                follow = request.POST['follow']
+                user = request.user
+                user.profile.following = user.profile.following + " " + follow
+                user.save()
+            except:
+                pass
 
-		if request.method == 'POST':
-			try:
-				postid = request.POST['share']
-				post = Post.objects.get(id=postid)
-				if request.user != post.author:	
-					newshare = Share.objects.create(shared=request.user)
-					newshare.postid = postid
-					newshare.save()
-					for user in User.objects.all():
-						if request.user.username in user.profile.following.split(" "):
-							user.profile.notifications = user.profile.notifications+1
-							user.save()
+            try:
+                postid = request.POST['share']
+                post = Post.objects.get(id=postid)
+                if request.user != post.author:
+                    newshare = Share.objects.create(shared=request.user)
+                    newshare.postid = postid
+                    newshare.save()
+                    for user in User.objects.all():
+                        if request.user.username in user.profile.following.split(" "):
+                            user.profile.notifications = user.profile.notifications + 1
+                            user.profile.notificationsString += "Post shared by " + str(request.user) + "|"
+                            user.save()
 
-			except Exception as e:
-				pass
-	
+            except Exception as e:
+                pass
+
+            try:
+
+                user = User.objects.get(pk=request.user.id)
+                temp = user.profile.following.split(" ")
+                temp.remove(request.POST['follow'])
+                user.profile.following = ' '.join(temp)
+                user.save()
+            except:
+                pass
+            try:
+                user = User.objects.get(pk=request.user.id)
+                post = Post.objects.get(pk=request.POST['unique'])
+                if post.author != user and request.POST['unique'] not in user.profile.liked:
+                    post.likes += 1
+                    post.save()
+                    user.profile.liked = user.profile.liked + " " + request.POST['unique']
+                    user.save()
+                elif post.author != user and request.POST['unique'] in user.profile.liked:
+                    temp = user.profile.liked.split(" ")
+                    temp.remove(request.POST['unique'])
+                    user.profile.liked = ' '.join(temp)
+                    post.likes -= 1
+                    post.save()
+                    user.save()
+            except:
+                pass
+            try:
+                comment = request.POST['placeholder']
+                newcomment = Comment.objects.create(author=request.user)
+                newcomment.post = Post.objects.get(pk=request.POST['submit'])
+                newcomment.text = comment
+                newcomment.save()
+                for user in User.objects.all():
+                    if request.user.username in user.profile.following.split(" "):
+                        user.profile.notifications = user.profile.notifications + 1
+                        user.save()
+            except:
+                pass
+        user = User.objects.get(pk=request.user.id)
+
+        posts = Post.objects.filter(author=request.user)
+        postlist = list(posts)
+        shares = Share.objects.filter(shared=request.user)
+        for z in shares:
+            content = Post.objects.get(id=z.postid)
+            content.author.username = content.author.username + ', Shared by ' + z.shared.username
+            content.created_date = z.date
+            postlist.append(content)
+            print(postlist)
+            print(set(postlist))
+        comments = Comment.objects.all()
+        if user.profile.following != "":
+            for i in set(user.profile.following.split(" ")):
+                if i != "":
+                    following = User.objects.get(username=i)
+                    otherposts = Post.objects.filter(author=following)
+                    posts = posts | otherposts
+                    postlist.extend(list(otherposts))
+                    sharedposts = Share.objects.filter(shared=following)
+                    for z in sharedposts:
+                        content = Post.objects.get(id=z.postid)
+                        content.author.username = content.author.username + ' Shared by ' + z.shared.username
+                        content.created_date = z.date
+                        postlist.append(content)
+        notificationsString = request.user.profile.notificationsString.split("|")
+        userlist = User.objects.exclude(pk=request.user.id)
+        context = {
+            'posts': postlist,
+            'comments': comments,
+            'FOLLOWING': set(user.profile.following.split(" ")[1:]),
+            'test': (user.profile.following.split(" ")),
+            'currentuser': request.user,
+            'notifications': notificationsString,
+            'users': userlist,
+        }
+
+        return render(request, "twitterclone/home.html", context)
+    else:
+        return redirect('login')
 
 
-			try:
-
-				user = User.objects.get(pk=request.user.id)
-				temp = user.profile.following.split(" ")
-				temp.remove(request.POST['follow'])
-				user.profile.following = ' '.join(temp)
-				user.save()
-			except:
-				pass
-			try:
-				user = User.objects.get(pk=request.user.id)
-				post = Post.objects.get(pk=request.POST['unique'])
-				if post.author != user and request.POST['unique'] not in user.profile.liked:
-					post.likes += 1
-					post.save()
-					user.profile.liked = user.profile.liked +" " + request.POST['unique']			
-					user.save()
-				elif post.author != user and request.POST['unique'] in user.profile.liked:
-					temp = user.profile.liked.split(" ")
-					temp.remove(request.POST['unique'])
-					user.profile.liked = ' '.join(temp)
-					post.likes -= 1
-					post.save()
-					user.save()
-			except: 
-				pass
-			try:
-				comment = request.POST['placeholder']
-				newcomment = Comment.objects.create(author=request.user)
-				newcomment.post = Post.objects.get(pk=request.POST['submit'])
-				newcomment.text = comment
-				newcomment.save()
-				for user in User.objects.all():
-					if request.user.username in user.profile.following.split(" "):
-						user.profile.notifications = user.profile.notifications + 1
-						user.save()
-			except:
-				pass			
-		user = User.objects.get(pk=request.user.id)
-		
-		posts = Post.objects.filter(author=request.user)
-		postlist = list(posts)
-		shares = Share.objects.filter(shared=request.user)
-		for z in shares:
-			content = Post.objects.get(id=z.postid)
-			content.author.username =content.author.username +  ', Shared by ' + z.shared.username
-			content.created_date = z.date
-			postlist.append(content)
-			print(postlist)
-			print(set(postlist))
-		comments = Comment.objects.all()
-		if user.profile.following != "":	
-			for i in set(user.profile.following.split(" ")):
-				if i != "":
-					following = User.objects.get(username=i)
-					otherposts = Post.objects.filter(author=following)
-					posts = posts | otherposts
-					postlist.extend(list(otherposts))
-					sharedposts = Share.objects.filter(shared=following)
-					for z in sharedposts:
-						content = Post.objects.get(id=z.postid)
-						content.author.username = content.author.username + ' Shared by ' + z.shared.username
-						content.created_date = z.date
-						postlist.append(content)
-		context = {
-		'posts':postlist,
-		'comments':comments,
-		'FOLLOWING' : set(user.profile.following.split(" ")[1:]),
-		'test' : (user.profile.following.split(" ")),	
-	}
-
-		return render(request,"twitterclone/home.html",context)
-	else:
-		return redirect('login')
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -117,26 +132,27 @@ def signup(request):
 
 
 def addpost(request):
-	if request.method == 'POST':
-		form = forms.PostForm(request.POST)
-		if form.is_valid():
-			instance = form.save(commit=False)
-			instance.author=request.user
-			instance.save()
-			return redirect('home')
-	else:
-		form = forms.PostForm()
+    if request.method == 'POST':
+        form = forms.PostForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.save()
+            return redirect('home')
+    else:
+        form = forms.PostForm()
 
-	return render(request,'twitterclone/create.html',{'form':form})
+    return render(request, 'twitterclone/create.html', {'form': form})
+
 
 def testfollow(request):
-	if request.method == 'POST':
-		user =  User.objects.get(pk=request.user.id)
-		user.profile.following = user.profile.following + " " +  request.POST['follow']
-		user.save()
-		
-	userlist = User.objects.exclude(pk=request.user.id)
-	context = {
-	'users':userlist
-	}
-	return render(request,'twitterclone/follow.html',context)		
+    if request.method == 'POST':
+        user = User.objects.get(pk=request.user.id)
+        user.profile.following = user.profile.following + " " + request.POST['follow']
+        user.save()
+
+    userlist = User.objects.exclude(pk=request.user.id)
+    context = {
+        'users': userlist
+    }
+    return render(request, 'twitterclone/follow.html', context)
