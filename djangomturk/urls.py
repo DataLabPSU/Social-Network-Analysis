9d937f91525e74939ad0cf2e5506a7de91a4210b
Jay <improcuziam@gmail.com>
2019-02-19 20:27:16 -0500

    fix static files for prod

--- djangomturk/urls.py
+++ djangomturk/urls.py
@@ -18,10 +18,17 @@ from django.urls import path
 from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
+from django.views.static import serve 
 
 urlpatterns = [
     path("", include('django.contrib.auth.urls')),
     path("", include('twitterclone.urls')),
+
+    #serving static files in production
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    
     #path('admin/', admin.site.urls),
 ]
 
