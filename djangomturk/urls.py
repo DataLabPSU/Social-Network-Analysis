12db39d268c5d5de8f61a1e5d6d57b28254aeedf
Jay <improcuziam@gmail.com>
2019-02-19 20:11:15 -0500

    update base db and ready for production

--- djangomturk/urls.py
+++ djangomturk/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", include('django.contrib.auth.urls')),
     path("", include('twitterclone.urls')),
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
