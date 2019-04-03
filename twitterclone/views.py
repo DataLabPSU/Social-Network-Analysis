b9d83d07f4b16f779119394d44adfe8102bb59e5
Jay <improcuziam@gmail.com>
2019-04-03 19:07:21 -0400

    fix cred update

--- twitterclone/views.py
+++ twitterclone/views.py
@@ -14,6 +14,7 @@ import random, os, json
 
 
 def updateCredibilityScore(request):
+	d = {}
 	for user in User.objects.all():
 		temp = 0.1 * (user.profile.real + 1) / (user.profile.real + user.profile.fake + 2)
 		temp2 = 1 - 0.1
