ce93ee12799810644bf6e03b545b144223034ba4
Jay <improcuziam@gmail.com>
2019-04-04 01:45:06 -0400

    fix templates dir

--- djangomturk/settings.py
+++ djangomturk/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'djangomturk.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['./templates','../templates', ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
